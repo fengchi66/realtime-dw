4cad02d5240e105b4f2c531812cb1c7197b7108b
wufengchi <wufengchi@yit.com>
2021-04-05 23:36:01 +0800

    add => module:mock-log

---
+++ mock-log/src/test/java/com/dmall/data/mocklog/MockLogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dmall.data.mocklog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MockLogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
